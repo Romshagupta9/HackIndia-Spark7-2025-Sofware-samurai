d6e3a4accf9bd338b90d9aa5885f652cf319e387
kamal-poudel <kamalpoudel138@gmail.com>
2025-04-28 22:56:06 +0530

    user auth done and mongodb is also connected!

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -17,12 +17,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+connectDB();
 
 // Server
 const PORT = process.env.PORT || 5000;
